0f374dbbb524c381e0a54cab00292dd196acd616
Yogev1102 <yogevbenari@gmail.com>
2025-06-20 18:37:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 VERIFICATION_TOKEN = "sellpilot_prod_verification_token_92ZxhG3lqpV8yRjKLtA7eXpJmFbNsWdHqC0UvYMtA9BrKw"
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/notifications", methods=["GET", "POST"])
 def ebay_verification():
     return VERIFICATION_TOKEN, 200
 
